4ad7e0aed6ba0d74cc2eade3a2da5a7ea6993b4f
Aarodynamics3 <hanrahanaaron@gmail.com>
2021-06-19 11:57:48 -0500

    Added a loop to main to continue entering multiple workouts.

--- Workout/WorkoutMain.cs
+++ Workout/WorkoutMain.cs
@@ -4,25 +4,35 @@ namespace Workout {
     class WorkoutMain {
         static void Main(string[] args) {
             String response;
+            bool endLoop = false;
 
-            Console.Write("Add a new workout? (y/n) ");
-            response = Console.ReadLine();
-            response = response.ToLower();
+            while (!endLoop) {
+                Console.Write("Add a new workout? (y/n) ");
+                response = Console.ReadLine();
+                response = response.ToLower();
 
-            if (response.Equals("y")) {
-                int week, day;
+                if (response.Equals("y")) {
+                    int week, day;
 
-                Console.Write("Week: ");
-                week = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Week: ");
+                    week = Convert.ToInt32(Console.ReadLine());
 
-                Console.Write("Day: ");
-                day = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Day: ");
+                    day = Convert.ToInt32(Console.ReadLine());
 
-                Workout _workout = new Workout(week, day);
+                    /*
+                     * TODO: Read in all of the previous workouts and ask the user
+                     * if they want to use the same exercises as the previous day
+                     * For example: if the user is inputting week 2 day 5, ask if they
+                     * are using the same exercises as week 1 day 5.
+                     */
 
-                Console.WriteLine(_workout.toDelimitedString());
-            } else if (response.Equals("n")) {
-                Console.WriteLine("No.");
+                    Workout _workout = new Workout(week, day);
+
+                    Console.WriteLine(_workout.toDelimitedString());
+                } else if (response.Equals("n")) {
+                    endLoop = true;
+                }
             }
         }
     }
